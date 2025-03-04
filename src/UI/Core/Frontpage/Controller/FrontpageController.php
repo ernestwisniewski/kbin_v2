823ab43af5149213c3cdc9675f90d94dba31f9b0
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2025-03-04 19:27:03 +0100

    Update entity mappings, init fronpage controller.

--- src/UI/Core/Frontpage/Controller/FrontpageController.php
+++ src/UI/Core/Frontpage/Controller/FrontpageController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kbin\UI\Core\Board;
+namespace Kbin\UI\Core\Frontpage\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,10 +9,8 @@ use Symfony\Component\Routing\Attribute\Route;
 final class FrontpageController extends AbstractController
 {
     #[Route('/frontpage', name: 'app_frontpage')]
-    public function index(): Response
+    public function __invoke(): Response
     {
-        return $this->render('frontpage/index.html.twig', [
-            'controller_name' => 'FrontpageController',
-        ]);
+        return $this->render('pages/frontpage.html.twig');
     }
 }
